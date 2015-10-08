b7acb201be45ac17b041c7f1446c1d69780ffd3f
lazite <774029165@qq.com>
2015-10-08 17:43:08 +0800

    创建usfor

---
+++ usfor/usfor/src/main/java/cn/usfor/dao/ResourceDao.java
@@ -0,0 +1,21 @@
+package cn.usfor.dao;
+
+import java.util.List;
+
+import cn.usfor.entity.Resource;
+
+/**
+ * <p>Resource: Zhang Kaitao
+ * <p>Date: 14-1-28
+ * <p>Version: 1.0
+ */
+public interface ResourceDao {
+
+    public Resource createResource(Resource resource);
+    public Resource updateResource(Resource resource);
+    public void deleteResource(Long resourceId);
+
+    Resource findOne(Long resourceId);
+    List<Resource> findAll();
+
+}
---
+++ usfor/usfor/src/main/java/cn/usfor/dao/UserDao.java
@@ -0,0 +1,24 @@
+package cn.usfor.dao;
+
+import java.util.List;
+
+import cn.usfor.entity.User;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 14-1-28
+ * <p>Version: 1.0
+ */
+public interface UserDao {
+
+    public User createUser(User user);
+    public User updateUser(User user);
+    public void deleteUser(Long userId);
+
+    User findOne(Long userId);
+
+    List<User> findAll();
+
+    User findByUsername(String username);
+
+}
